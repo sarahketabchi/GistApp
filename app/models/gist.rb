79c33d76e90e1f0142d72df58539a68676841edd
Shaun + Sarah <shaun+sarah@appacademy.io>
2013-02-18 11:15:58 -0800

    initial commit

---
+++ app/models/gist.rb
@@ -0,0 +1,3 @@
+class Gist < ActiveRecord::Base
+  attr_accessible :title, :user_id
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GistApp::Application.config.secret_token = '4aba38165b087b3cf9939d8175cd29c602d0604e48f6d0f7c427eb4737b7dc50242866510abe71004048588d7387da013e6107fcdf644ced56d9b156d62d8d92'
